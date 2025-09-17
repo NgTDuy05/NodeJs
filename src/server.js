8274d9c82ccd0d47db2641417905a43d8dc10d29
NgTDuy05 <duynguyen.th2004@gmail.com>
2025-09-17 09:55:22 +0700

    edit server

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express')
 const configViewEngine = require('./config/viewEngine');
 const webRoutes = require('./routes/web')
+const mysql = require('mysql2')
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -15,6 +16,23 @@ const port = process.env.PORT || 3000
 configViewEngine(app);
 
 app.use('/', webRoutes);
+
+
+//test connection
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3307,
+    user: 'root',
+    password: '123456',
+    database: 'nguyenduy0512'
+})
+connection.query(
+    'SELECT * FROM Users u',
+    function (err, result, fields) {
+        console.log(">>>result= ", result);
+        console.log(">>>fields= ", fields);
+    }
+)
 
 app.listen(3000, '0.0.0.0', () => {
     console.log(`Example app listening on port ${port}`)
